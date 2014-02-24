3dcc91ee77ec7188f91711a02f2bf56d76c4c0c8
hal9ccc <hal9ccc@googlemail.com>
2014-02-24 03:21:18 +0100

    code39 integriert

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,5 +24,61 @@ angular.module('ngPrintingApp', [
 .directive('hu',  function() { return { templateUrl: 'templates/label-hu.html'  }; })
 
 .run(function ($rootScope, $http, jobProvider) {
-  $rootScope.someData = {message: "hello"};
+  //$rootScope.someData = {message: "hello"};
+
+  //generateBarcode();
+
 });
+
+
+function generateBarcode(pValue, pType, pRenderer){
+  var value     = pValue    || 'undefined';
+  var btype     = pType     || 'code39';
+  var renderer  = pRenderer || 'css';
+  var quietZone = false;
+
+  var settings = {
+    output:     renderer,
+    bgColor:    "#FFFFFF",
+    color:      "#000000",
+    barWidth:    1,
+    barHeight:  50,
+    moduleSize:  5,
+    posX:       10,
+    posY:       20,
+    addQuietZone:1
+  };
+  //if ($("#rectangular").is(':checked') || $("#rectangular").attr('checked')){
+  //  value = {code:value, rect: true};
+  //}
+
+  //if (renderer == 'canvas'){
+  //  clearCanvas();
+  //  $("#barcodeTarget").hide();
+  //  $("#canvasTarget").show().barcode(value, btype, settings);
+  //} else {
+  //  $("#canvasTarget").hide();
+  $("#barcodeTarget").html("").show().barcode(value, btype, settings);
+  //}
+}
+
+function showConfig1D(){
+  $('.config .barcode1D').show();
+  $('.config .barcode2D').hide();
+}
+
+function showConfig2D(){
+  $('.config .barcode1D').hide();
+  $('.config .barcode2D').show();
+}
+
+function clearCanvas(){
+  var canvas = $('#canvasTarget').get(0);
+  var ctx = canvas.getContext('2d');
+  ctx.lineWidth = 1;
+  ctx.lineCap = 'butt';
+  ctx.fillStyle = '#FFFFFF';
+  ctx.strokeStyle  = '#000000';
+  ctx.clearRect (0, 0, canvas.width, canvas.height);
+  ctx.strokeRect (0, 0, canvas.width, canvas.height);
+}
---
+++ app/scripts/directives/code39.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('ngPrintingApp')
+  .directive('code39', function () {
+    return {
+      template: '<div>333</div>',
+      scope: {
+        v1: '@txt',
+        v2: '=txt',
+        v3: '&txt'
+      },      restrict: 'EA',
+      link: function postLink(scope, element, attrs) {
+        var btype     = 'code39';
+        var renderer  = 'css';
+        var quietZone = false;
+        var t = element[0] ? element[0].innerText : '';
+
+        var num = scope.$eval(attrs.txt);
+
+        var settings = {
+          output:     renderer,
+          bgColor:    "#FFFFFF",
+          color:      "#000000",
+          barWidth:    1,
+          barHeight:  50,
+          moduleSize:  5,
+          posX:       10,
+          posY:       20,
+          addQuietZone:1
+        };
+
+
+        //angular.element(element).barcode(scope.$eval(attrs.directiveName));
+        angular.element(element).barcode('444444444444', btype, settings);
+      }
+    };
+  });
