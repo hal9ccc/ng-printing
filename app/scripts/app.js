eb3d3706a6d8d44a7f6c8b66f3fe3f0a582c132d
hal9ccc <hal9ccc@googlemail.com>
2014-03-15 20:04:34 +0100

    funzt wieder

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,84 +1,90 @@
 'use strict';
 
 angular.module('ngPrintingApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-.config(function ($routeProvider, $locationProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/main.html',
-      controller: 'MainCtrl'
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        //$locationProvider.html5Mode(true);
     })
-    .otherwise({
-      redirectTo: '/'
+
+    .directive('vda', function () {
+        return { templateUrl: 'templates/doc-vda.html' };
+    })
+    .directive('uwa', function () {
+        return { templateUrl: 'templates/doc-uwa.html' };
+    })
+    .directive('hu', function () {
+        return { templateUrl: 'templates/doc-hu.html'  };
+    })
+
+    .run(function ($rootScope, $http, jobProvider) {
+        //$rootScope.someData = {message: "hello"};
+
+        //generateBarcode();
+
     });
 
-    //$locationProvider.html5Mode(true);
-})
 
-.directive('vda', function() { return { templateUrl: 'templates/label-vda.html' }; })
-.directive('uwa', function() { return { templateUrl: 'templates/label-uwa.html' }; })
-.directive('hu',  function() { return { templateUrl: 'templates/label-hu.html'  }; })
+function generateBarcode(pValue, pType, pRenderer) {
+    var value = pValue || 'undefined';
+    var btype = pType || 'code39';
+    var renderer = pRenderer || 'css';
+    var quietZone = false;
 
-.run(function ($rootScope, $http, jobProvider) {
-  //$rootScope.someData = {message: "hello"};
+    var settings = {
+        output: renderer,
+        bgColor: "#FFFFFF",
+        color: "#000000",
+        barWidth: 1,
+        barHeight: 50,
+        moduleSize: 5,
+        posX: 10,
+        posY: 20,
+        addQuietZone: 1
+    };
+    //if ($("#rectangular").is(':checked') || $("#rectangular").attr('checked')){
+    //  value = {code:value, rect: true};
+    //}
 
-  //generateBarcode();
-
-});
-
-
-function generateBarcode(pValue, pType, pRenderer){
-  var value     = pValue    || 'undefined';
-  var btype     = pType     || 'code39';
-  var renderer  = pRenderer || 'css';
-  var quietZone = false;
-
-  var settings = {
-    output:     renderer,
-    bgColor:    "#FFFFFF",
-    color:      "#000000",
-    barWidth:    1,
-    barHeight:  50,
-    moduleSize:  5,
-    posX:       10,
-    posY:       20,
-    addQuietZone:1
-  };
-  //if ($("#rectangular").is(':checked') || $("#rectangular").attr('checked')){
-  //  value = {code:value, rect: true};
-  //}
-
-  //if (renderer == 'canvas'){
-  //  clearCanvas();
-  //  $("#barcodeTarget").hide();
-  //  $("#canvasTarget").show().barcode(value, btype, settings);
-  //} else {
-  //  $("#canvasTarget").hide();
-  $("#barcodeTarget").html("").show().barcode(value, btype, settings);
-  //}
+    //if (renderer == 'canvas'){
+    //  clearCanvas();
+    //  $("#barcodeTarget").hide();
+    //  $("#canvasTarget").show().barcode(value, btype, settings);
+    //} else {
+    //  $("#canvasTarget").hide();
+    $("#barcodeTarget").html("").show().barcode(value, btype, settings);
+    //}
 }
 
-function showConfig1D(){
-  $('.config .barcode1D').show();
-  $('.config .barcode2D').hide();
+function showConfig1D() {
+    $('.config .barcode1D').show();
+    $('.config .barcode2D').hide();
 }
 
-function showConfig2D(){
-  $('.config .barcode1D').hide();
-  $('.config .barcode2D').show();
+function showConfig2D() {
+    $('.config .barcode1D').hide();
+    $('.config .barcode2D').show();
 }
 
-function clearCanvas(){
-  var canvas = $('#canvasTarget').get(0);
-  var ctx = canvas.getContext('2d');
-  ctx.lineWidth = 1;
-  ctx.lineCap = 'butt';
-  ctx.fillStyle = '#FFFFFF';
-  ctx.strokeStyle  = '#000000';
-  ctx.clearRect (0, 0, canvas.width, canvas.height);
-  ctx.strokeRect (0, 0, canvas.width, canvas.height);
+function clearCanvas() {
+    var canvas = $('#canvasTarget').get(0);
+    var ctx = canvas.getContext('2d');
+    ctx.lineWidth = 1;
+    ctx.lineCap = 'butt';
+    ctx.fillStyle = '#FFFFFF';
+    ctx.strokeStyle = '#000000';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
 }
